ecafca4ae9751f58fe6f7913c7eec7f86262174c
Cnu89 <107422396+Cnu89@users.noreply.github.com>
2022-07-13 09:51:26 +0530

    Update Private.java
    Adding add method

--- src/Accessmodifier/Private.java
+++ src/Accessmodifier/Private.java
@@ -7,9 +7,15 @@ public class Private {
         int c=a*b;
         System.out.println(c);
     }
+    Private static void add(){
+        int a=33;
+        int b=44;
+        int c=a+b;
+       System.out.println(c);
 
     public static void main(String[] args) {
         Private.mul();
+        Private.add();
 
     }
 
